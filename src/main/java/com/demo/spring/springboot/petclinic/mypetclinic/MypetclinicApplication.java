3ad923cf25bb61096e3a6ebd61f7a3d226751f9a
balaji13us <Balaji>
2018-08-23 21:08:00 +0530

    initial

---
+++ src/main/java/com/demo/spring/springboot/petclinic/mypetclinic/MypetclinicApplication.java
@@ -0,0 +1,12 @@
+package com.demo.spring.springboot.petclinic.mypetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MypetclinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MypetclinicApplication.class, args);
+    }
+}
